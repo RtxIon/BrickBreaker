08f431e153086da5116a4f29a3a373a1b293e66b
RtxIon <romangorin28@gmail.com>
2024-03-03 17:31:19 -0800

    need 2 work on what happens when you collide with the brick

--- BrickBreaker/Form1.cs
+++ BrickBreaker/Form1.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Data;
 using System.Windows.Forms;
+using System.Windows.Forms.Automation;
 
 namespace BrickBreaker
 {
@@ -8,6 +9,19 @@ namespace BrickBreaker
     public partial class Form1 : Form
     {
         List<List<Rectangle>> bricks = new List<List<Rectangle>>();
+        HashSet<Keys> pressedKeys;
+        List<Rectangle> blocks = new List<Rectangle>();
+        int brickWidth = 150;
+        int brickHeight = 50;
+        int rowCount = 3;
+        int lives = 3;
+        Graphics gfx;
+        Bitmap bmp;
+        Point speed = new Point(10, 10);
+        Rectangle ball;
+        Rectangle paddle;
+        Rectangle block;
+
 
         bool intersects(Rectangle a, Rectangle b)
         {
@@ -22,38 +36,31 @@ namespace BrickBreaker
                 return false;
             }
         }
-
-
+        
 
         void createBricks()
         {
-            for(int j = 0; j < ((pictureBox1.Bottom) - pictureBox1.Bottom/2) - 1; j++)
+            for(int j = 0; j < rowCount; j++)
             {
+                
                 List<Rectangle> row = new List<Rectangle>();
-                for(int i = 0; i < pictureBox1.Right /50; i++)
+                for(int i = 0; i < pictureBox1.Right /brickWidth; i++)
                 {
-                    Rectangle box = new Rectangle(i, j * 50, 50, 50);
+                    Rectangle box = new Rectangle(i*brickWidth, j*brickHeight, brickWidth, brickHeight);
                     row.Add(box);
                 }
                 bricks.Add(row);
             }
         }
 
-        Graphics gfx;
-        Bitmap bmp;
-        Point speed = new Point(15, 15);
-        HashSet<Keys> pressedKeys;
-        List<Rectangle> blocks = new List<Rectangle>();
-        Rectangle ball;
-        Rectangle paddle;
-        Rectangle block;
-
+       
         public Form1()
         {
             InitializeComponent();
             paddle = new Rectangle(360, 389, 80, 15);
             ball = new Rectangle(384, 318, 30, 30);
             block = new Rectangle(7, 9, 180, 50);
+            
 
             bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
 
@@ -63,50 +70,47 @@ namespace BrickBreaker
             pressedKeys = new HashSet<Keys>();
 
             createBricks();
-            for (int i = 0; i < bricks.Count; i++)
-            {
-                for (int j = 0; j < bricks[i].Count; j++)
-                {
-                    gfx.FillRectangle(Brushes.DarkSlateGray, bricks[i][j]);
-                    gfx.FillRectangle(Brushes.DarkGray, bricks[i][j].X - 3, bricks[i][j].Y - 3, bricks[i][j].Width + 6, bricks[i][j].Height + 6);
-                }
-
-            }
-
-           
-
-            //Form.Sizechange // runs whenever the size of the form is changed.
-            //https://planetcalc.com/8989/ <- This resource may help in sacaling things to the size of the form (screen).
-            /*
-             * scale factor for brick size can be this: 
-             * 1. get ratio: currentScreenHeight/brickHeight & currentScreenWidth/brickWidth
-             * 2. scale: newScreenHeight/brickHeight, newScreeWidth/brickWidth. currentBrickProportions*quotients
-             * */
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            label1.Text = $"{lives}";
             gfx.Clear(Color.BlanchedAlmond);
             gfx.FillRectangle(Brushes.DarkGray, block.X - 3, block.Y - 3, block.Width+6, block.Height+6);
             gfx.FillRectangle(Brushes.LightCyan, block);
             gfx.FillRectangle(Brushes.BurlyWood, paddle);
             gfx.FillEllipse(Brushes.RosyBrown, ball);
 
-           
+            for (int i = 0; i < bricks.Count; i++)
+            {
+                for (int j = 0; j < bricks[i].Count; j++)
+                {
+                    gfx.FillRectangle(Brushes.DarkSlateGray, bricks[i][j]);
+                    gfx.DrawRectangle(Pens.Gray, bricks[i][j].X - 3, bricks[i][j].Y - 3, bricks[i][j].Width + 3, bricks[i][j].Height + 3);
+                }
+
+            }
+
             ball.X += speed.X;//speed.X;
             ball.Y += speed.Y;//speed.Y;
 
-            if (ball.Top <= 0 || ball.Bottom >= ClientSize.Height)
+            if (ball.Top <= 0)
             {
-                speed.Y *= -1;
+                speed.Y = Math.Abs(speed.Y);
+                
+            }
+            if(ball.Bottom >= ClientSize.Height)
+            {
+                lives--;
+                speed.Y = -Math.Abs(speed.Y);
             }
             if (ball.Left <= 0)
             {
-                speed.X *= -1;
+                speed.X = -Math.Abs(speed.X);
             }
             else if (ball.Right >= ClientSize.Width)
             {
-                speed.X *= -1;
+                speed.X = -Math.Abs(speed.X);
             }
             if (intersects(ball, paddle))
             { 
@@ -128,6 +132,16 @@ namespace BrickBreaker
                     paddle.X += 15;
                 }
                 
+            }
+
+            for(int i = 0; i < bricks.Count; i++) {
+                for(int j = 0; j  < bricks[i].Count; j++)
+                if (bricks[i][j].IntersectsWith(ball))
+                {
+                    gfx.FillRectangle(Brushes.Maroon, bricks[i][j]);
+                        //speed.Y = -Math.Abs(speed.Y); 
+                        speed.Y *= -1;
+                }
             }
 
             pictureBox1.Image = bmp;
